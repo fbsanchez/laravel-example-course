8d3a99d9e8025c7997bc237622323d55bab107d0
fbsanchez <fborja.sanchez@artica.es>
2022-03-03 11:51:02 +0100

    some minor change

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -8,16 +8,17 @@
     <div>
         <h1 style="text-align: left">{{ $title }}</h1>
 
+        @if (isset($nav) && is_array($nav))
+            <nav style="text-align: left; margin-bottom: 2em;">
 
-        <nav style="text-align: left; margin-bottom: 2em;">
-            @if (isset($nav) && is_array($nav))
                 @foreach ($nav as $route => $content)
                     <x-button href="{{ route($route) }}">
                         {{ $content }}
                     </x-button>
                 @endforeach
-            @endif
-        </nav>
+
+            </nav>
+        @endif
 
         <div>
             {{ $slot }}
